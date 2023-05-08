29382f93dc3a7ad890f5a2ff34e8a8a64b2f598c
aubinleyoung <aubinsimpeze01@gmail.com>
2023-05-08 22:29:41 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :likes
+  
   resources :users, only: [:index, :show] do
   resources :posts, only: [:index, :show]
   # root 'pages#home'
