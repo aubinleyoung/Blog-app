228a21bd66a29bcc197dc8f0507c68ac8a9ab5d5
aubinleyoung <aubinsimpeze01@gmail.com>
2023-05-11 18:47:03 +0200

    Display all users and single user's post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,12 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @user = User.find(params[:user_id])
-    @post = @user.posts
+    @post = Post.all
   end
 
   # GET /posts/1 or /posts/1.json
   def show
     @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
   end
 end
