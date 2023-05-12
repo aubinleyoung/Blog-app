04cc9c818290b1e7253f316286e6687c71c0c79d
aubinleyoung <aubinsimpeze01@gmail.com>
2023-05-12 16:06:22 +0200

    Implement method for creating comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-
   def new
     @comment = Comment.new
     @post = Post.find(params[:post_id])
@@ -9,17 +8,18 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params)
     @post = Post.find(params[:post_id])
     @comment.post_id = @post.id
-
     if @comment.save
-      flash[:success] = "comment added successfully"
+      flash[:success] = 'Comment created successfully'
     else
       flash[:danger] = "Couldn't create comment"
     end
     redirect_to "/users/#{current_user.id}/posts/#{params[:post_id]}"
   end
-   private
+
+  private
+
   def comment_params
     params.require(:comment).permit(:content)
   end
-
 end
+
