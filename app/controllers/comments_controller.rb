7a3d314b1a339eef0b5a5c0e148d2dd4acbee546
aubinleyoung <aubinsimpeze01@gmail.com>
2023-05-12 14:19:50 +0200

    Add commit comments and likes controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+    @post = Post.find(params[:post_id])
+  end
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment.post_id = @post.id
+
+    if @comment.save
+      flash[:success] = "comment added successfully"
+    else
+      flash[:danger] = "Couldn't create comment"
+    end
+    redirect_to "/users/#{current_user.id}/posts/#{params[:post_id]}"
+  end
+   private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   # GET /users or /users.json
   def index
     @users = User.all.order(created_at: :asc)
+    @current_user = current_user
   end
 
   # GET /users/1 or /users/1.json
