29eca3cbf54e314c87326401b57a077241aa8591
aubinleyoung <aubinsimpeze01@gmail.com>
2023-05-18 14:56:03 +0200

    Fix user logs bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ApplicationRecord
-  
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :posts, foreign_key: :author_id, class_name: 'Post'
   has_many :likes, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
 
-
-  validates :name, presence: true
+  # validates :name, presence: true
   validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def three_recent_posts
